3cc91e7e394ca0e6d7ca3ffd38e68f5bae83a565
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-10-08 08:44:08 -0500

    Creando test jugadores mayores de 34 años

---
+++ challenge-reactor-core/src/test/java/com/example/demo/CSVUtilTest.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CSVUtilTest {
+
+    private final String team ="Real madrid";
+
+    @Test
+    void converterData(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        assert list.size() == 18207;
+    }
+
+    @Test
+    void stream_filtrarJugadoresMayoresA35(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Map<String, List<Player>> listFilter = list.parallelStream()
+                .filter(player -> player.age >= 35)
+                .map(player -> {
+                    player.name = player.name.toUpperCase(Locale.ROOT);
+                    return player;
+                })
+                .flatMap(playerA -> list.parallelStream()
+                        .filter(playerB -> playerA.club.equals(playerB.club))
+                )
+                .distinct()
+                .collect(Collectors.groupingBy(Player::getClub));
+
+        assert listFilter.size() == 322;
+    }
+
+
+    @Test
+    void reactive_filtrarJugadoresMayoresA34(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .filter(player -> player.age >= 34)
+                .filter(player -> player.club.equals(team))
+                .distinct()
+                .collectMultimap(Player::getClub);
+        System.out.println(listFilter.block().size());
+        listFilter.block().forEach((s, players)->{
+            System.out.println(s);
+            players.forEach(player -> {
+                System.out.println(player.name +" "+ player.age);
+                assert player.club.equals(team);
+            });
+        });
+        assert listFilter.block().size() == 1;
+    }
+
+
+}
