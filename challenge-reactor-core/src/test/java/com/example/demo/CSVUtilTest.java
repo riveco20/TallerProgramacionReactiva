a6f5e3e42538d571d33102171a7fbf5c3b984c35
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-10-08 09:02:59 -0500

    Agregando filtro por nacionalidad

--- challenge-reactor-core/src/test/java/com/example/demo/CSVUtilTest.java
+++ challenge-reactor-core/src/test/java/com/example/demo/CSVUtilTest.java
@@ -64,5 +64,28 @@ public class CSVUtilTest {
         assert listFilter.block().size() == 1;
     }
 
+    @Test
+    void reactive_filterNacionalitYRancking() {
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .buffer(100)
+                .flatMap(playerA -> listFlux
+                        .filter(playerB -> playerA.stream()
+                                .anyMatch(a -> a.national.equals(playerB.national)))
+                ).distinct()
+                .sort((k, player) -> player.winners)
+                .collectMultimap(Player::getNational);
+
+        System.out.println("En total con " + listFilter.block().size() + "Pais");
+        System.out.println("Por Nacionalidad es: ");
+        listFilter.block().forEach((pais, players) -> {
+            System.out.println("Pais " + pais);
+            players.forEach(player -> {
+                System.out.println(player.name +" " + player.winners + " victorias ");
+            });
+        });
+    }
+
 
 }
